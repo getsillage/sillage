7c2f7137041011482152f8fc9819433766dc925b
樊彧 <fanyu.felix@gmail.com>
2026-07-12 11:22:54 +0800

    chore(android): release version 0.1.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.sillage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
     }
 
     signingConfigs {

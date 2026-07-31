484943052a5ad845ce7b6ebfc05a2b36fdb53f0f
Yu Fan <hi@miofelix.com>
2026-07-31 22:45:41 +0800

    chore(release): prepare v0.3.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.sillage"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.3.0"
+        versionCode = 11
+        versionName = "0.3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
